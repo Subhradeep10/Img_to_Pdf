fc31669916c256733712c2a20143b9db626be161
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-06-21 20:52:42 +0530

    Changed JS file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var Imagefile = document.getElementById("Imagefile");
+var Imagefile = document.getElementById("Imagefile")
 
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas")
 
-var button = document.getElementById("button");
+var button = document.getElementById("button")
+
+Imagefile.addEventListener("change",handleImage, false)
